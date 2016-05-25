7b5778b6a55128e9e0e331e3d9eeae610d82221f
ごま <gomayumax@users.noreply.github.com>
2016-05-26 00:46:55 +0900

    土日対応

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+var Shukjitz = require('shukjitz'); 
 function Ochingin(day) {
   this.day = day;
 }
 
 Ochingin.prototype.check = function(date) {
-  if(this.day === date.getDate()){
-    return true;
-  } else {
-    return false;
+  if(isYasumi(date, this.day) === true) {
+    if(date.getDay() === 5 ){
+      return true;
+    }
+  } else{
+    if(this.day === date.getDate()){
+      return true;
+    } 
   }
+    return false;
 };
+
+// 給料日が土日かチェック
+function isYasumi(date, day){
+  var ochingin_date = new Date(date.getTime());
+  ochingin_date.setDate(day);
+    // 土日
+  if(ochingin_date.getDay() == 0 || ochingin_date.getDay() == 6) {
+        return true;
+  }
+  return false;
+}
 
 module.exports = Ochingin;
